16976309e843584562bbfd5dfae9041e1c57cb10
Raja <45028516+RajaTheResidentAlien@users.noreply.github.com>
2022-02-15 16:58:33 -0800

    added play button
    added easy-to-see/activate play/stop button

--- lib/scrn.lua
+++ lib/scrn.lua
@@ -9,11 +9,14 @@ function redraw()
   screen.move(0,12)
   screen.font_size(8)
   screen.text(params:get("clock_tempo")) 
-  if U1shft==2 then screen.level(15) else screen.level(8) end
+  if U1shft==1 then screen.level(15) else screen.level(8) end
+  screen.move(20,8)
+  if go>0 then screen.circle(26,8.5,4) else screen.rect(23,6,6,6) end screen.stroke()
+  if U1shft==3 then screen.level(15) else screen.level(8) end
   screen.move(44,16)
   screen.font_size(7)
   screen.text("Loops: "..params:get("loops"))
-  if U1shft==3 then screen.level(15) else screen.level(8) end
+  if U1shft==4 then screen.level(15) else screen.level(8) end
   screen.font_size(7)
   screen.move(0,22)
   screen.text('File:')
@@ -22,7 +25,7 @@ function redraw()
   screen.text(x[whichfile])
   screen.move(38,8)
   screen.font_size(7)
-  if U1shft==1 then screen.level(15) else screen.level(8) end
+  if U1shft==2 then screen.level(15) else screen.level(8) end
   screen.text("FileDiv: "..params:get("file_div"))
   screen.move(88, 8)
   screen.font_size(6)
@@ -33,26 +36,26 @@ function redraw()
   screen.text(bar..":"..(count+1))
   screen.font_size(6)
   screen.move(0, 40)
-  if U1shft==4 then screen.level(15) else screen.level(8) end
+  if U1shft==5 then screen.level(15) else screen.level(8) end
     local fln = 0
     if params:get("fnl")>0 then fln="on" else fln="off" end
   screen.text("FNL: "..fln) --"file name lock"
   screen.move(0, 47)
-  if U1shft==5 then screen.level(15) else screen.level(8) end
+  if U1shft==6 then screen.level(15) else screen.level(8) end
     local fvx = 0
     if params:get("fxv")>0 then fvx="on" else fvx="off" end
   screen.text("FXV: "..fvx) --"fx vortex"
   screen.move(32, 40)
-  if U1shft==6 then screen.level(15) else screen.level(8) end
+  if U1shft==7 then screen.level(15) else screen.level(8) end
   screen.text("TRG: "..params:get("trg")) --"trigger" resolution(recording)
   screen.move(32, 47)
-  if U1shft==7 then screen.level(15) else screen.level(8) end
+  if U1shft==8 then screen.level(15) else screen.level(8) end
   screen.text("VOX: "..params:get("vox")) --"voice" number(softcut...to record into)
   screen.move(64, 40)
-  if U1shft==8 then screen.level(15) else screen.level(8) end
+  if U1shft==9 then screen.level(15) else screen.level(8) end
   screen.text("RLN: "..params:get("rln")) --"recording length"(in 'counts'(quarter-notes))
   screen.move(64, 47)
-  if U1shft==9 then screen.level(15) else screen.level(8) end
+  if U1shft==10 then screen.level(15) else screen.level(8) end
     local lcr = 0
     if params:get("lrc")>0 then lcr="on" else lcr="off" end
   screen.text("LRC: "..lcr) --"loop recording"
